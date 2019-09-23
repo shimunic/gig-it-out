a75752cc43e1c4fa70c8d9be4032e34cff3d9015
Shamil Gadzhiev <shimunic@gmail.com>
2019-09-23 19:51:16 +0300

    changed creds

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('./config/passport')(passport);
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb+srv://shimunic%40gmail.com:35335kagdilishkI%26%29@cluster0-zsqfy.mongodb.net/test?retryWrites=true&w=majority',
+    'mongodb+srv://shimshim:WucyRr5rBiaNnRcn@cluster0-zsqfy.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true },
 
   )
